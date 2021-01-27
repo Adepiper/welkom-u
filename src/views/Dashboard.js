d9e4b39f0bf01a8a7eb5003ebef87c825cbcb822
adepiper <tobi@roadpreppers.com>
2021-01-27 18:33:16 +0100

    dashboard responsive

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,34 +1,47 @@
+import flat from './images/flat.svg';
+import Group from './images/Group.svg';
+
 const Dashboard = () => {
   return (
     <div className='dashboard-container'>
       <div className='dashboard-cards'>
         <div className='row'>
           <div className='row'>
-            <div className='col-md-4'>
+            <div className='col-lg-6'>
               <div className='card currency'>
+                <div className='card-img'>
+                  <img src={flat} alt='' />
+                </div>
                 <div className='card-info'>
                   <h4>Exchange Currency</h4>
                   <p>Exchange money from one</p>
                   <p>local currency to another</p>
                 </div>
-                <div className='card-img'></div>
               </div>
             </div>
-            <div className='col-md-4 '>
+            <div className='col-md-6 '>
               <div className='card accomodation'>
-                <h4>Get an Accomodation</h4>
-                <p>Find the best accomodation</p>
-                <p>at afordable price.</p>
+                <div className='card-img'>
+                  <img src={flat} alt='' />
+                </div>
+                <div className='card-info'>
+                  <h4>Exchange Currency</h4>
+                  <p>Exchange money from one</p>
+                  <p>local currency to another</p>
+                </div>
               </div>
-              <div className='card-img'></div>
             </div>
-            <div className='col-md-4'>
+            <div className='col-md-6'>
               <div className='card explore'>
-                <h4>Explore your destination</h4>
-                <p>Get relevant information about</p>
-                <p>any country and community</p>
+                <div className='card-img'>
+                  <img src={flat} alt='' />
+                </div>
+                <div className='card-info'>
+                  <h4>Exchange Currency</h4>
+                  <p>Exchange money from one</p>
+                  <p>local currency to another</p>
+                </div>
               </div>
-              <div className='card-img'></div>
             </div>
           </div>
         </div>
